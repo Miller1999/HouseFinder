729e9f246be3f2befeb655ec831c960465f57118
Miller1999 <millerjavierarias@gmail.com>
2023-07-29 01:52:19 -0500

    Corregidos problemas con la barra de navegacion
    Se añadio el cambio dependiendo de lo que se busque, es decir, si filtra por ciudad o por cantidad de personas

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -22,17 +22,21 @@ const StyledDiv = styled.div`
     display:inline-flex;
     align-items:center;
     border-radius: 20px;
-    width:350px;
+    width:300px;
     height:50px;
     box-shadow: 1px 1px 10px 0px rgba(0,0,0,0.75);
 `
-const StyledSpan = styled.span`
-    font-size: 12px;
+const NavDiv = styled(StyledDiv)`
+    width:100%;
+    justify-content:space-evenly;
 `
-const StyledInput = styled.input`
-    width:60%;
-    border:none;
-    outline:none;
+    
+
+const StyledSpan = styled.p`
+    font-size: 16px;
+`
+const GraySpan = styled(StyledSpan)`
+    color:gray;
 `
 const PDiv = styled.div`
     padding:10px;
@@ -55,16 +59,16 @@ const CenterDiv = styled(PDiv)`
     border-right: 1px lightgray solid;
     border-left: 1px lightgray solid;
     padding: 10px 0px;
+    width:40%;
 `
 const StyledNav = styled.nav`
     width:100%;
     height:35vh;
     background-color:white;
-    backdrop-filter: drop-shadow(16px 16px 10px black);
+    position:relative;
+    border-radius: 20px;
+    box-shadow: 5px 5px 0px 1000px rgba(0,0,0,0.7);
 
-    &:first-child, div{
-        width:100%;
-    }
 `
 const Lista = styled(StyledDiv)`
     width: 100%;
@@ -79,58 +83,105 @@ const ElementoLista = styled(PDiv)`
     justify-content: start;
     gap:10px;
 `
+const Guests = styled.div`
+    position: absolute;
+    left: 35%;
+    top: 75px;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    width:50%;
+`
+const BotonesSR = styled.div`
+    display:flex;
+    align-items:center;
+    gap:10px;
+
+`
+
 const Header = () =>{
-    const [mostrarOcultar,setMostrar] = useState(false)
+    const [mostrarCities,setMostrarCities] = useState(false)
+    const [mostrarGuests,setMostrarGuests] = useState(false)
     const [location,setlocation] = useState("Helsinki")
     const mostrar = () => {
-        setMostrar(!mostrarOcultar)
+        setMostrarCities(!mostrarCities)
+        setMostrarGuests(false)
+    }
+    const mostrarG = () => {
+        setMostrarGuests(!mostrarGuests)
+        setMostrarCities(false)
     }
     const enviarID = (e) => {
         setlocation(e.target.id) 
     }
-    return(
-        <StyledHeader>
-        {
-            mostrarOcultar ?  <StyledNav>
+    if(mostrarCities){
+        return(<StyledHeader>
+                <StyledNav>
+                <NavDiv>
+                <PDiv>
+                    <StyledButton onClick = {mostrar}>{location}, Finland</StyledButton>
+                </PDiv>
+                <CenterDiv>
+                <StyledButton onClick = {mostrarG}>Add Guests</StyledButton>
+                </CenterDiv>
+                <PDiv>
+                    <StyledButton><SearchIcon/></StyledButton>
+                </PDiv>
+            </NavDiv>
+            <Lista>
+            {
+                cities.map((city,index) => {
+                    return <ElementoLista key={index} ><LocationOnIcon sx={{color:colorResaltado}}/><StyledButton id={city} onClick={enviarID}>{city}, Finland</StyledButton></ElementoLista>
+                })
+            }
+            </Lista>
+            </StyledNav>
+        </StyledHeader>)
+    } else if(mostrarGuests){
+        return(<StyledHeader>
+                <StyledNav>
+                <NavDiv>
+                <PDiv>
+                    <StyledButton onClick = {mostrar}>{location}, Finland</StyledButton>
+                </PDiv>
+                <CenterDiv>
+                <StyledButton onClick = {mostrarG}>Add Guests</StyledButton>
+                </CenterDiv>
+                <PDiv>
+                    <StyledButton><SearchIcon/></StyledButton>
+                </PDiv>
+            </NavDiv>
+            <Guests>
+            <StyledSpan>Adults</StyledSpan>
+            <GraySpan>Ages 13 or above</GraySpan>
+            <BotonesSR><button>-</button><span>0</span><button>+</button></BotonesSR>
+            <StyledSpan>Children</StyledSpan>
+            <GraySpan>Ages 2-12</GraySpan>
+            <BotonesSR><button>-</button><span>0</span><button>+</button></BotonesSR>
+        </Guests>
+        </StyledNav>
+        </StyledHeader>)
+    }
+    else{
+        return(
+            <StyledHeader>
+            <Fragment>
+            <StyledLogo src={Logo} alt="logo"/>
             <StyledDiv>
-            <PDiv>
-                <StyledButton onClick = {mostrar}>{location}, Finland</StyledButton>
-            </PDiv>
-            <CenterDiv>
-                <StyledInput type="text" placeholder="Add Guests"/>
-            </CenterDiv>
-            <PDiv>
-                <StyledButton><SearchIcon/></StyledButton>
-            </PDiv>
-        </StyledDiv>
-        <Lista>
-        {
-            cities.map((city,index) => {
-                return <ElementoLista key={index} ><LocationOnIcon sx={{color:colorResaltado}}/><StyledButton id={city} onClick={enviarID}>{city}, Finland</StyledButton></ElementoLista>
-            })
-        }
-        </Lista>
-        </StyledNav> 
-        :<Fragment>
-        <StyledLogo src={Logo} alt="logo"/>
-        <StyledDiv>
-            <PDiv>
-                <StyledButton onClick = {mostrar}>{location}, Finland</StyledButton>
-            </PDiv>
-            <CenterDiv>
-                <StyledInput type="text" placeholder="Add Guests"/>
-            </CenterDiv>
-            <PDiv>
-                <StyledButton><SearchIcon/></StyledButton>
-            </PDiv>
-        </StyledDiv>
-        </Fragment>       
-
-        }
-       
-
-        </StyledHeader>
-    )
+                <PDiv>
+                    <StyledButton onClick = {mostrar}>{location}, Finland</StyledButton>
+                </PDiv>
+                <CenterDiv>
+                <StyledButton onClick = {mostrarG}>Add Guests</StyledButton>
+                </CenterDiv>
+                <PDiv>
+                    <StyledButton><SearchIcon/></StyledButton>
+                </PDiv>
+            </StyledDiv>
+            </Fragment>       
+            </StyledHeader>
+        )
+    }
 }
 
 export default Header
